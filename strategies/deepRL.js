73101e0d017a02cb9a96f230dcc37b5de22d5fae
dennohtu <dennismureithi10@gmail.com>
2019-05-09 08:16:39 +0300

    init commit

---
+++ strategies/deepRL.js
@@ -0,0 +1,25 @@
+var strat = {}
+
+console.log('hello world')
+
+strat.init = function(){
+
+}
+
+strat.update = function(candle){
+
+}
+
+strat.log = function(){
+
+}
+
+strat.check = function(){
+    this.advice('long')
+}
+
+strat.end = function(){
+
+}
+
+module.exports = strat;
